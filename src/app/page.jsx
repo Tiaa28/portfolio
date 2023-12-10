86ac77a91e1a8ccc33482bf754c7c48151dba4f5
Tiaa28 <tniandari@gmail.com>
2023-12-10 14:24:55 +0700

    create heros section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
+import Hero from "@/sections/Hero";
+
 export default function Home() {
   return (
     <main className="container mx-auto md:px-0 px-4">
-      <section className="h-screen">qwryiwuegroiuq3gr</section>
+      <Hero />
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -11,6 +12,8 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        gradient: 'linear-gradient(to left, #079911, #107720)',
+        'gradient-hover': 'linear-gradient(to right, #079911, #107720)',
       },
     },
     colors: {
@@ -19,7 +22,7 @@ module.exports = {
         white: '#fff',
         black: '#232323',
         grey: '#728196',
-        'green-light': '#EAF5EB'
+        'green-light': '#EAF5EB',
       }
     }
   },
